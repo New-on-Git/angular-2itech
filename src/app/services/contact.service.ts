1ca0bffae2264bac9e57077435da1dbd8e9238fe
Yvan DARMET <cmoiyvan@gmail.com>
2021-11-10 15:09:41 +0100

    j'ai oublié de nommer le dernier commit : Mise en place de l'envoie des infos du form de contact dans le data.json -> création d'un service pour ça + création de la route vers le local server

---
+++ src/app/services/contact.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Products} from "../model/Products";
+import {environment} from "../../environments/environment";
+import {Contact} from "../model/Contact";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+  constructor(private http: HttpClient) {
+
+  }
+  postContact(contact:Contact):Observable<Contact> {
+     return this.http.post<Contact>(environment.API.baseUrl + environment.API.endpoint.contact , contact);
+  }
+}
